5871fa7f1378559cc8d685aa80b1cb44f6a607e4
Norbiros <norbiros@protonmail.com>
2023-11-24 19:54:53 +0100

    feat: `SqlUUID` database type

---
+++ src/main/java/org/spacemc/utils/database/SqlUUID.kt
@@ -0,0 +1,25 @@
+package org.spacemc.utils.database
+
+import org.ktorm.schema.BaseTable
+import org.ktorm.schema.Column
+import org.ktorm.schema.SqlType
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.sql.Types
+import java.util.*
+
+object UuidSqlType : SqlType<UUID>(Types.OTHER, "uuid") {
+
+    override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: UUID) {
+        ps.setObject(index, parameter.toString())
+    }
+
+    override fun doGetResult(rs: ResultSet, index: Int): UUID? {
+        val uuid = rs.getObject(index) as String? ?: return null
+        return UUID.fromString(uuid)
+    }
+}
+
+fun BaseTable<*>.uuid(name: String): Column<UUID> {
+    return registerColumn(name, UuidSqlType)
+}
